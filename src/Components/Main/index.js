7eec0bff70e45757f4512c8754db81a9721031ef
Jessie Tarrosa <creators.nuke@gmail.com>
2020-03-19 17:36:24 +0800

    style: change coding style

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -6,26 +6,24 @@ import { Box } from '@material-ui/core'
 export const Main = () => {
   const { todos } = useContext(TodoContext)
 
-  const todoList = todos.length ? (
-    <Fragment>
-      {todos.map(todo => (
-        <TodoList 
-          key={todo.id}
-          id={todo.id}
-          content={todo.content}
-          isCompleted={todo.isCompleted}
-        />
-      ))}
-    </Fragment>
-  ) : (
-    <Box display='flex' alignItems='center' justifyContent='center'>
-      No more Todo&apos;s left!!!
-    </Box>
-  )
-
   return (
     <Fragment>
-      {todoList}
+      {todos.length ? (
+        <Fragment>
+          {todos.map(todo => (
+            <TodoList 
+              key={todo.id}
+              id={todo.id}
+              content={todo.content}
+              isCompleted={todo.isCompleted}
+            />
+          ))}
+        </Fragment>
+      ) : (
+        <Box display='flex' alignItems='center' justifyContent='center'>
+          No more Todo&apos;s left!!!
+        </Box>
+      )}
     </Fragment>
   )
 }
